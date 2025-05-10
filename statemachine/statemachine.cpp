2a81842837d76637c4a33cd84f67c59b1d1c45f3
Manh Lam Luu <luumanhlam59@gmail.com>
2025-05-10 11:20:31 +0200

    Fix state scope problem

--- statemachine/statemachine.cpp
+++ statemachine/statemachine.cpp
@@ -1,194 +1,202 @@
 #include <string>
 #include "..\Control\Control.h"
 
-enum States {st_init, ortho, ortho_L, ortho_LL, ortho_R, ortho_RR, diag_LR, diag_RL, diag_RR,diag_LL,st_stop};
+enum class States {
+    st_init, ortho, ortho_L, ortho_R, st_stop, ortho_LL, ortho_RR, diag_LR, diag_RL, diag_RR, diag_LL
+};
 
-// State machine function
 void statemachine(const std::string& robot_commands) {
-    int x = 0; // Initialize x
-    States current_st = st_init;
+    int x = 0;
+    States current_st = States::st_init;
 
     for (char cmd : robot_commands) {
         switch (cmd) {
             case 'F':
                 switch (current_st) {
-                    case st_init:
-                        current_st = ortho;
+                    case States::st_init:
+                        current_st = States::ortho;
                         x = 1;
                         break;
-                    case ortho:
-                        current_st = ortho;
+                    case States::ortho:
                         x++;
                         break;
-                    case ortho_L:
-                        current_st = ortho;
+                    case States::ortho_L:
+                        current_st = States::ortho;
                         SS90L();
                         x = 2;
                         break;
-                    case ortho_R:
-                        current_st = ortho;
+                    case States::ortho_R:
+                        current_st = States::ortho;
                         SS90R();
                         x = 2;
                         break;
-                    case ortho_LL:
-                        current_st = ortho;
+                    case States::ortho_LL:
+                        current_st = States::ortho;
                         SS180L();
                         x = 2;
                         break;
-                    case ortho_RR:
-                        current_st = ortho;
+                    case States::ortho_RR:
+                        current_st = States::ortho;
                         SS180R();
                         x = 2;
                         break;
-                    case diag_LR:
-                        current_st = ortho;
+                    case States::diag_LR:
+                        current_st = States::ortho;
                         DIA(x);
                         DS45R();
                         x = 2;
                         break;
-                    case diag_RL:
-                        current_st = ortho;
+                    case States::diag_RL:
+                        current_st = States::ortho;
                         DIA(x);
                         DS45L();
                         x = 2;
                         break;
-                    case diag_LL:
-                        current_st = ortho;
+                    case States::diag_LL:
+                        current_st = States::ortho;
                         DIA(x);
                         DS135L();
                         x = 2;
                         break;
-                    case diag_RR:
-                        current_st = ortho;
+                    case States::diag_RR:
+                        current_st = States::ortho;
                         DIA(x);
                         DS135R();
                         x = 2;
+                        break;
+                    default:
                         break;
                 }
                 break;
 
             case 'L':
                 switch (current_st) {
-                    case ortho:
-                        current_st = ortho_L;
+                    case States::ortho:
+                        current_st = States::ortho_L;
                         FWD(x);
                         break;
-                    case ortho_L:
-                        current_st = ortho_LL;
+                    case States::ortho_L:
+                        current_st = States::ortho_LL;
                         break;
-                    case ortho_R:
-                        current_st = diag_RL;
+                    case States::ortho_R:
+                        current_st = States::diag_RL;
                         SD45R();
                         x = 2;
                         break;
-                    case ortho_RR:
-                        current_st = diag_RL;
+                    case States::ortho_RR:
+                        current_st = States::diag_RL;
                         SD135R();
                         x = 2;
                         break;
-                    case diag_LR:
-                        current_st = diag_RL;
+                    case States::diag_LR:
+                        current_st = States::diag_RL;
                         x++;
                         break;
-                    case diag_RL:
-                        current_st = diag_LL;
+                    case States::diag_RL:
+                        current_st = States::diag_LL;
                         break;
-                    case diag_RR:
-                        current_st = diag_RL;
+                    case States::diag_RR:
+                        current_st = States::diag_RL;
                         DIA(x);
                         DD90R();
                         x = 2;
+                        break;
+                    default:
                         break;
                 }
                 break;
 
             case 'R':
                 switch (current_st) {
-                    case ortho:
-                        current_st = ortho_R;
+                    case States::ortho:
+                        current_st = States::ortho_R;
                         FWD(x);
                         break;
-                    case ortho_R:
-                        current_st = ortho_RR;
+                    case States::ortho_R:
+                        current_st = States::ortho_RR;
                         break;
-                    case ortho_L:
-                        current_st = diag_LR;
+                    case States::ortho_L:
+                        current_st = States::diag_LR;
                         SD45L();
                         x = 2;
                         break;
-                    case ortho_LL:
-                        current_st = diag_LR;
+                    case States::ortho_LL:
+                        current_st = States::diag_LR;
                         SD135L();
                         x = 2;
                         break;
-                    case diag_RL:
-                        current_st = diag_LR;
+                    case States::diag_RL:
+                        current_st = States::diag_LR;
                         x++;
                         break;
-                    case diag_LR:
-                        current_st = diag_RR;
+                    case States::diag_LR:
+                        current_st = States::diag_RR;
                         break;
-                    case diag_LL:
-                        current_st = diag_LR;
+                    case States::diag_LL:
+                        current_st = States::diag_LR;
                         DIA(x);
                         DD90L();
                         x = 2;
+                        break;
+                    default:
                         break;
                 }
                 break;
 
             case 'S':
                 switch (current_st) {
-                    case st_init:
-                        current_st = st_stop;
+                    case States::st_init:
+                        current_st = States::st_stop;
                         break;
-                    case ortho:
-                        current_st = st_stop;
+                    case States::ortho:
+                        current_st = States::st_stop;
                         FWD(x);
                         break;
-                    case ortho_L:
-                        current_st = st_stop;
+                    case States::ortho_L:
+                        current_st = States::st_stop;
                         SS90L();
                         FWD(1);
                         break;
-                    case ortho_R:
-                        current_st = st_stop;
+                    case States::ortho_R:
+                        current_st = States::st_stop;
                         SS90R();
                         FWD(1);
                         break;
-                    case ortho_LL:
-                        current_st = st_stop;
+                    case States::ortho_LL:
+                        current_st = States::st_stop;
                         SS180L();
                         FWD(1);
                         break;
-                    case ortho_RR:
-                        current_st = st_stop;
+                    case States::ortho_RR:
+                        current_st = States::st_stop;
                         SS180R();
                         FWD(1);
                         break;
-                    case diag_LR:
-                        current_st = st_stop;
+                    case States::diag_LR:
+                        current_st = States::st_stop;
                         DIA(x);
                         DS45R();
                         FWD(1);
                         break;
-                    case diag_RL:
-                        current_st = st_stop;
+                    case States::diag_RL:
+                        current_st = States::st_stop;
                         DIA(x);
                         DS45L();
                         FWD(1);
                         break;
-                    case diag_LL:
-                        current_st = st_stop;
+                    case States::diag_LL:
+                        current_st = States::st_stop;
                         DIA(x);
                         DS135L();
                         FWD(1);
                         break;
-                    case diag_RR:
-                        current_st = st_stop;
+                    case States::diag_RR:
+                        current_st = States::st_stop;
                         DIA(x);
                         DS135R();
                         FWD(1);
+                        break;
+                    default:
                         break;
                 }
                 break;
--- statemachine/statemachine_simplified.cpp
+++ statemachine/statemachine_simplified.cpp
@@ -1,97 +1,102 @@
 #include <string>
 #include "..\Control\Control.h"
 
-enum States {st_init, ortho, ortho_L, ortho_R,st_stop};
+enum class SimpleStates { st_init, ortho, ortho_L, ortho_R, st_stop };
 
-void simplestatemachine(const std::string& robot_commands){
-    int x;
-    States current_st = st_init;
-    for (char cmd : robot_commands){
+void simplestatemachine(const std::string& robot_commands) {
+    int x = 0;
+    SimpleStates simple_current_st = SimpleStates::st_init;
+
+    for (char cmd : robot_commands) {
         switch (cmd) {
             case 'F':
-                switch (current_st) {
-                    case st_init:
-                        current_st = ortho;
+                switch (simple_current_st) {
+                    case SimpleStates::st_init:
+                        simple_current_st = SimpleStates::ortho;
                         x = 1;
                         break;
-                    case ortho:
-                        current_st = ortho;
+                    case SimpleStates::ortho:
+                        simple_current_st = SimpleStates::ortho;
                         x++;
                         break;
-                    case ortho_L:
-                        current_st = ortho;
+                    case SimpleStates::ortho_L:
+                        simple_current_st = SimpleStates::ortho;
                         SS90L();
                         x = 2;
                         break;
-                    case ortho_R:
-                        current_st = ortho;
+                    case SimpleStates::ortho_R:
+                        simple_current_st = SimpleStates::ortho;
                         SS90R();
                         x = 2;
                         break;
-
+                    default:
+                        break;
                 }
                 break;
+
             case 'L':
-                switch (current_st) {
-                    case ortho:
-                        current_st = ortho_L;
+                switch (simple_current_st) {
+                    case SimpleStates::ortho:
+                        simple_current_st = SimpleStates::ortho_L;
                         FWD(x);
                         break;
-					case ortho_L:
-						current_st = ortho_L;
-						SS90L;
-						FWD(1);
-						break;
-					case ortho_R:
-                        current_st = ortho_L;
-						SS90R;
-						FWD(1);
-						break;
-
+                    case SimpleStates::ortho_L:
+                        simple_current_st = SimpleStates::ortho_L;
+                        SS90L();
+                        FWD(1);
+                        break;
+                    case SimpleStates::ortho_R:
+                        simple_current_st = SimpleStates::ortho_L;
+                        SS90R();
+                        FWD(1);
+                        break;
+                    default:
+                        break;
                 }
                 break;
 
             case 'R':
-                switch (current_st) {
-                    case ortho:
-                        current_st = ortho_R;
+                switch (simple_current_st) {
+                    case SimpleStates::ortho:
+                        simple_current_st = SimpleStates::ortho_R;
                         FWD(x);
                         break;
-					case ortho_R:
-						current_st = ortho_R;
-						SS90R;
-						FWD(1);
-						break;
-					case ortho_L:
-                        current_st = ortho_R;
-						SS90L;
-						FWD(1);
-						break;
-
-                }
-                break;
-
-
-            case 'S':
-                switch (current_st) {
-                    case st_init:
-                        current_st = st_stop;
-                        break;
-                    case ortho:
-                        current_st = st_stop;
-                        FWD(x);
-                        break;
-                    case ortho_L:
-                        current_st = st_stop;
-                        SS90L();
-                        FWD(1);
-                        break;
-                    case ortho_R:
-                        current_st = st_stop;
+                    case SimpleStates::ortho_R:
+                        simple_current_st = SimpleStates::ortho_R;
                         SS90R();
                         FWD(1);
                         break;
+                    case SimpleStates::ortho_L:
+                        simple_current_st = SimpleStates::ortho_R;
+                        SS90L();
+                        FWD(1);
+                        break;
+                    default:
+                        break;
+                }
+                break;
 
+            case 'S':
+                switch (simple_current_st) {
+                    case SimpleStates::st_init:
+                        simple_current_st = SimpleStates::st_stop;
+                        break;
+                    case SimpleStates::ortho:
+                        simple_current_st = SimpleStates::st_stop;
+                        FWD(x);
+                        break;
+                    case SimpleStates::ortho_L:
+                        simple_current_st = SimpleStates::st_stop;
+                        SS90L();
+                        FWD(1);
+                        break;
+                    case SimpleStates::ortho_R:
+                        simple_current_st = SimpleStates::st_stop;
+                        SS90R();
+                        FWD(1);
+                        break;
+                    default:
+                        break;
                 }
                 break;
         }
